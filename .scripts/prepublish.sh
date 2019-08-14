65940abde4ac12ff0cfc21d0f9e174418a73fbd4
Dioris Moreno <dmoreno@libersys.io>
2019-08-14 14:21:48 -0500

    Package dependencies updated

--- .scripts/prepublish.sh
+++ .scripts/prepublish.sh
@@ -1,19 +1,18 @@
-#!\bin\bash
+#!/bin/bash
 
 # IMPORTANT
 # ---------
 # This is an auto generated file with React CDK.
 # Do not modify this file.
-# Use `.scripts\user\prepublish.sh instead`.
+# Use `.scripts/user/prepublish.sh instead`.
 
 echo "=> Transpiling 'src' into ES5 ..."
 echo ""
-rm -rf .\dist
-SET NODE_ENV=production
-node_modules\.bin\babel --ignore tests,stories .\src --out-dir .\dist
-node_modules\.bin\webpack .\dist\index.js .\dist\bundled.js
-node_modules\.bin\uglifyjs dist\bundled.js -o dist\bundled.min.js -m
+rm -rf ./dist
+NODE_ENV=production ./node_modules/.bin/babel --ignore tests,stories ./src --out-dir ./dist
+NODE_ENV=production ./node_modules/.bin/webpack ./dist/index.js ./dist/bundled.js
+NODE_ENV=production ./node_modules/.bin/uglifyjs dist/bundled.js -o dist/bundled.min.js -m
 echo ""
 echo "=> Transpiling completed."
 
-. .scripts\user\prepublish.sh
+. .scripts/user/prepublish.sh
